0cb726afc2a0f5307db5215f39cea393f17532c3
Daniel Bünzli <daniel.buenzli@erratique.ch>
2014-03-23 22:05:11 +0100

    Add test/js_test a JavaScript leak test.

---
+++ test/js_test.ml
@@ -0,0 +1,47 @@
+(* Test for ~strong stop *)
+
+open React
+
+(* Artificially increase memory usage *) 
+let high_e e = 
+  let id e = E.map (fun v -> v) e in 
+  id @@ id @@ id @@ id @@ id @@ id @@ id @@ id @@ id @@ id @@ id @@ id @@
+  id @@ id @@ id @@ id @@ id @@ id @@ id @@ id @@ id @@ id @@ id @@ id @@
+  id @@ id @@ id @@ id @@ id @@ id @@ id @@ id @@ id @@ id @@ id @@ id @@
+  id @@ id @@ id @@ id @@ id @@ id @@ id @@ id @@ id @@ id @@ id @@ id @@
+  id @@ id @@ id @@ id @@ id @@ id @@ id @@ id @@ id @@ id @@ id @@ id @@
+  id @@ id @@ id @@ id @@ id @@ id @@ id @@ id @@ id @@ id @@ id @@ id @@
+  id @@ id @@ id @@ id @@ id @@ id @@ id @@ id @@ id @@ id @@ id @@ id @@
+  id @@ id @@ id @@ id @@ id @@ id @@ id @@ id @@ id @@ id @@ id @@ id @@
+  id @@ id @@ id @@ id @@ id @@ id @@ id @@ id @@ id @@ id @@ id @@ id @@
+  id @@ id @@ id @@ id @@ id @@ id @@ id @@ id @@ id @@ id @@ id @@ id @@
+  id @@ id @@ id @@ id @@ id @@ id @@ id @@ id @@ id @@ id @@ id @@ id @@
+  id @@ id @@ id @@ id @@ id @@ id @@ id @@ id @@ id @@ id @@ id @@ id @@
+  id @@ id @@ id @@ id @@ id @@ id @@ id @@ id @@ id @@ id @@ id @@ id @@
+  id @@ id @@ id @@ id @@ id @@ id @@ id @@ id @@ id @@ id @@ id @@ id @@
+  id @@ id @@ id @@ id @@ id @@ id @@ id @@ id @@ id @@ id @@ id @@ id @@
+  id @@ id @@ id @@ id @@ id @@ id @@ id @@ id @@ id @@ id @@ id @@ id @@
+  e
+
+let counter_ui = 
+  let none () = assert false in
+  let el = Dom_html.window ## document ## getElementById (Js.string "count") in
+  Js.Opt.get el none
+
+let count = ref 0
+let incr_counter () = 
+  incr count; 
+  counter_ui ## innerHTML <- Js.string (string_of_int !count)
+  
+let tick, send_tick = E.create ()
+
+let rec loop () = 
+  let ev = E.map (fun () -> incr_counter ()) (high_e tick) in
+  send_tick ();
+  E.stop ev;
+  ignore (Dom_html.window ## setTimeout (Js.wrap_callback loop, 1.))
+  
+
+let main _ = loop (); Js._false
+
+let () = Dom_html.window ## onload <- Dom_html.handler main
