ef1e8c38520ad11f85b88f4191adcf64a654f2c9
Daniel Bünzli <daniel.buenzli@erratique.ch>
2020-06-18 23:07:24 +0200

    Remove some watermarks.

--- src/react.ml
+++ src/react.ml
@@ -1,7 +1,6 @@
 (*---------------------------------------------------------------------------
    Copyright (c) 2009 Daniel C. Bünzli. All rights reserved.
    Distributed under the ISC license, see terms at the end of the file.
-   %%NAME%% %%VERSION%%
   ---------------------------------------------------------------------------*)
 
 let err_max_rank = "maximal rank exceeded"
--- src/react_top_init.ml
+++ src/react_top_init.ml
@@ -1,7 +1,6 @@
 (*---------------------------------------------------------------------------
    Copyright (c) 2014 Daniel C. Bünzli. All rights reserved.
    Distributed under the ISC license, see terms at the end of the file.
-   %%NAME%% %%VERSION%%
   ---------------------------------------------------------------------------*)
 
 open React
